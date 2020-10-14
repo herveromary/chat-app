01039fd0098e7a3163c20fd34f7d6d23a48a54e2
Hervé Romary <h.romary@gmail.com>
2020-10-14 10:29:33 +0200

    state added

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,21 +1,38 @@
 import React from "react";
 import "./Contact.css";
 
-function Contact(props) {
-  return (
-    <div className='Contact'>
-      <img className='avatar' src={props.avatar} alt='photo de profil' />
-      <div className='name'>
-        <p>{props.name}</p>
-        <div className='status'>
-          <div
-            className={props.online ? "status-online" : "status-offline"}
-          ></div>
-          <p className='status-text'>{props.online ? "online" : "offline"}</p>
+class Contact extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      online: false,
+    };
+  }
+
+  render() {
+    return (
+      <div className='Contact'>
+        <img className='avatar' src={this.props.avatar} alt='photo de profil' />
+        <div
+          onClick={() => {
+            const onlineStatus = !this.state.online;
+            this.setState({ online: onlineStatus });
+          }}
+          className='name'
+        >
+          <p>{this.props.name}</p>
+          <div className='status'>
+            <div
+              className={this.state.online ? "status-online" : "status-offline"}
+            ></div>
+            <p className='status-text'>
+              {this.state.online ? "online" : "offline"}
+            </p>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Contact;
